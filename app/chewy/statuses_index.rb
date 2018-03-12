b8a6d72dce7fcc8752484a8c9a9910607149fd8e
aquarla <satosamu@gmail.com>
2018-03-12 16:35:24 +0900

    Add support for Japanese full-text searching

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -5,60 +5,47 @@ class StatusesIndex < Chewy::Index
 
   settings index: index_preset(refresh_interval: '30s', number_of_shards: 5), analysis: {
     filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
+      custom_synonym: {
+        type: 'synonym',
+        synonyms_path: '/etc/elasticsearch/synonym.txt'
       },
-
-      english_stemmer: {
-        type: 'stemmer',
-        language: 'english',
-      },
-
-      english_possessive_stemmer: {
-        type: 'stemmer',
-        language: 'possessive_english',
+    },
+    tokenizer: {
+      ja_tokenizer: {
+        type: 'kuromoji_tokenizer',
+        mode: 'search',
+        user_dictionary: '/etc/elasticsearch/userdict_ja.txt',
       },
     },
 
     analyzer: {
-      verbatim: {
-        tokenizer: 'uax_url_email',
-        filter: %w(lowercase),
-      },
-
       content: {
-        tokenizer: 'standard',
+        tokenizer: 'ja_tokenizer',
+        type: 'custom',
+        char_filter: %w(
+          icu_normalizer
+        ),
         filter: %w(
-          lowercase
-          asciifolding
-          cjk_width
-          elision
-          english_possessive_stemmer
-          english_stop
-          english_stemmer
+          kuromoji_stemmer
+          kuromoji_part_of_speech
+          ja_stop
+          custom_synonym
         ),
       },
-
-      hashtag: {
-        tokenizer: 'keyword',
-        filter: %w(
-          word_delimiter_graph
-          lowercase
-          asciifolding
-          cjk_width
-        ),
+      ja_default_analyzer: {
+        tokenizer: 'kuromoji_tokenizer',
       },
     },
   }
 
   index_scope ::Status.unscoped.kept.without_reblogs.includes(:media_attachments, :local_mentioned, :local_favorited, :local_reblogged, :local_bookmarked, :tags, preview_cards_status: :preview_card, preloadable_poll: :local_voters), delete_if: ->(status) { status.searchable_by.empty? }
 
+
   root date_detection: false do
     field(:id, type: 'long')
     field(:account_id, type: 'long')
-    field(:text, type: 'text', analyzer: 'verbatim', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
-    field(:tags, type: 'text', analyzer: 'hashtag',  value: ->(status) { status.tags.map(&:display_name) })
+    field(:text, type: 'text', analyzer: 'ja_default_analyzer', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
+    field(:tags, type: 'text', analyzer: 'content',  value: ->(status) { status.tags.map(&:display_name) })
     field(:searchable_by, type: 'long', value: ->(status) { status.searchable_by })
     field(:language, type: 'keyword')
     field(:properties, type: 'keyword', value: ->(status) { status.searchable_properties })

fc21ba766dc93e98ca9c455297197e976b80c024
aquarla <satosamu@gmail.com>
2020-01-27 18:18:20 +0900

    Add PGroonga suppport.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_013357) do
+ActiveRecord::Schema.define(version: 2020_01_27_063006) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgroonga"
   enable_extension "plpgsql"
 
   create_table "account_aliases", force: :cascade do |t|
@@ -683,6 +684,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_013357) do
     t.index ["in_reply_to_account_id"], name: "index_statuses_on_in_reply_to_account_id"
     t.index ["in_reply_to_id"], name: "index_statuses_on_in_reply_to_id"
     t.index ["reblog_of_id", "account_id"], name: "index_statuses_on_reblog_of_id_and_account_id"
+    t.index ["text"], name: "index_statuses_on_text", using: :pgroonga
     t.index ["uri"], name: "index_statuses_on_uri", unique: true
   end
 

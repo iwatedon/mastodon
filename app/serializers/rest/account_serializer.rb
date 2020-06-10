ef99163448f69ff64d5668ba19fd417f5e074a09
aquarla <satosamu@gmail.com>
2020-06-10 15:30:18 +0900

    Revert "Account temporary muting."
    This reverts commit 2714f0e562eeb23a56d1cab3e3cfb601aac557cd.

--- app/serializers/rest/account_serializer.rb
+++ app/serializers/rest/account_serializer.rb
@@ -7,8 +7,6 @@ class REST::AccountSerializer < ActiveModel::Serializer
              :note, :url, :avatar, :avatar_static, :header, :header_static,
              :followers_count, :following_count, :statuses_count, :last_status_at
 
-  attribute :mute_expires_at, if: :current_user?
-
   has_one :moved_to_account, key: :moved, serializer: REST::AccountSerializer, if: :moved_and_not_nested?
   has_many :emojis, serializer: REST::CustomEmojiSerializer
 
@@ -21,10 +19,6 @@ class REST::AccountSerializer < ActiveModel::Serializer
   end
 
   has_many :fields
-
-  def current_user?
-    defined?(current_user) && !current_user.nil?
-  end
 
   def id
     object.id.to_s
@@ -64,9 +58,5 @@ class REST::AccountSerializer < ActiveModel::Serializer
 
   def last_status_at
     object.last_status_at&.to_date&.iso8601
-  end
-
-  def mute_expires_at
-    current_user.account&.mute_relationships&.find_by(target_account_id: object.id)&.expires_at
   end
 end

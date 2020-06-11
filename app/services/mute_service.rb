f52c7981be54f2ed06c6a18d5f8f72258175ebaf
aquarla <satosamu@gmail.com>
2020-06-11 15:16:58 +0900

    Revert "Add mute duration."
    This reverts commit 22a9aa2b69773bcb0d8e66bc54e2a92bd67c7765.

--- app/services/mute_service.rb
+++ app/services/mute_service.rb
@@ -1,20 +1,15 @@
 # frozen_string_literal: true
 
 class MuteService < BaseService
-  def call(account, target_account, notifications: nil, duration: 0)
+  def call(account, target_account, notifications: nil)
     return if account.id == target_account.id
 
-    mute = account.mute!(target_account, notifications: notifications, duration: duration)
+    mute = account.mute!(target_account, notifications: notifications)
 
     if mute.hide_notifications?
       BlockWorker.perform_async(account.id, target_account.id)
     else
       MuteWorker.perform_async(account.id, target_account.id)
-    end
-
-    if duration != 0
-      jid = DeleteMuteWorker.perform_at(duration.seconds, account.id, target_account.id)
-      mute.update!(unmute_jid: jid)
     end
 
     mute
--- db/migrate/20200318064228_add_unmute_jid_to_mute.rb
+++
@@ -1,5 +0,0 @@
-class AddUnmuteJidToMute < ActiveRecord::Migration[5.2]
-  def change
-    add_column :mutes, :unmute_jid, :string
-  end
-end

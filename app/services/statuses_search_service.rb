9ff24b456ffdf7986bce399cf82b81434fa5ef75
aquarla <satosamu@gmail.com>
2021-10-05 17:49:47 +0900

    Add media only FTL searching.

--- app/services/statuses_search_service.rb
+++ app/services/statuses_search_service.rb
@@ -16,8 +16,9 @@ class StatusesSearchService < BaseService
 
   def status_search_results
     results = Status.where(visibility: :public)
+                    .joins(:media_attachments)
                     .where('statuses.text &@~ ?', @query)
-                    .searchable_by_account(@account)
+                    .group(:id)
                     .offset(@offset)
                     .limit(@limit)
                     .order('statuses.id DESC')

93ed55930dd33289527856c48557185147a2c66c
aquarla <satosamu@gmail.com>
2022-06-10 17:13:19 +0900

    Fix comment

--- app/javascript/mastodon/features/hashtag_timeline/components/column_settings.js
+++ app/javascript/mastodon/features/hashtag_timeline/components/column_settings.js
@@ -43,7 +43,7 @@ class ColumnSettings extends React.PureComponent {
   onSelect = mode => value => {
     const oldValue = this.tags(mode);
 
-    // Prevent changes that add more than 4 tags, but allow removing
+    // Prevent changes that add more than 20 tags, but allow removing
     // tags that were already added before
     if ((value.length > 20) && !(value < oldValue)) {
       return;

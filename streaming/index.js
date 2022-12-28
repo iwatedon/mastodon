93ca55491975a2f66c3aee1197555588a2d6630e
aquarla <satosamu@gmail.com>
2022-12-28 16:43:51 +0900

    Replace local timeline with iwatedon tag timeline

--- streaming/index.js
+++ streaming/index.js
@@ -970,7 +970,7 @@ const startWorker = async (workerId) => {
       break;
     case 'public:local':
       resolve({
-        channelIds: ['timeline:public:local'],
+        channelIds: [`timeline:hashtag:${normalizeHashtag('iwatedon')}`],
         options: { needsFiltering: true },
       });
 
@@ -991,7 +991,7 @@ const startWorker = async (workerId) => {
       break;
     case 'public:local:media':
       resolve({
-        channelIds: ['timeline:public:local:media'],
+        channelIds: [`timeline:hashtag:${normalizeHashtag('iwatedon')}:media`],
         options: { needsFiltering: true },
       });
 

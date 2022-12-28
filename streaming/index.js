3c8b5aa7ee0b2c509b6f415d9db46256d0f556b0
aquarla <satosamu@gmail.com>
2022-12-28 16:43:51 +0900

    Replace local timeline with iwatedon tag timeline

--- streaming/index.js
+++ streaming/index.js
@@ -1106,7 +1106,7 @@ const startServer = async () => {
       resolveFeed('public', 'timeline:public', { needsFiltering: true });
       break;
     case 'public:local':
-      resolveFeed('public', 'timeline:public:local', { needsFiltering: true });
+      resolveFeed('hashtag', `timeline:hashtag:${normalizeHashtag('iwatedon')}`, { needsFiltering: true });
       break;
     case 'public:remote':
       resolveFeed('public', 'timeline:public:remote', { needsFiltering: true });
@@ -1115,7 +1115,7 @@ const startServer = async () => {
       resolveFeed('public', 'timeline:public:media', { needsFiltering: true });
       break;
     case 'public:local:media':
-      resolveFeed('public', 'timeline:public:local:media', { needsFiltering: true });
+      resolveFeed('hashtag', `timeline:hashtag:${normalizeHashtag('iwatedon')}:media`, { needsFiltering: true });
       break;
     case 'public:remote:media':
       resolveFeed('public', 'timeline:public:remote:media', { needsFiltering: true });

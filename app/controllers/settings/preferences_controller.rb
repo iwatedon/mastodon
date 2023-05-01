7e6a6ff95c6e0c94b305b88f85c01c201f10d10c
aquarla <satosamu@gmail.com>
2023-05-01 15:42:01 +0900

    Fix error

--- app/controllers/settings/preferences_controller.rb
+++ app/controllers/settings/preferences_controller.rb
@@ -55,8 +55,8 @@ class Settings::PreferencesController < Settings::BaseController
       :setting_trends,
       :setting_crop_images,
       :setting_always_send_emails,
-      notification_emails: %i(follow follow_request reblog favourite mention digest report pending_account trending_tag appeal),
       :setting_x_only_media_on_home_timeline,
+      notification_emails: %i(follow follow_request reblog favourite mention digest report pending_account trending_tag appeal),
       interactions: %i(must_be_follower must_be_following must_be_following_dm)
     )
   end

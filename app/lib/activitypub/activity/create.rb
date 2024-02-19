e4d3dcbdb06c09021d5df610df898a86e4c36be9
aquarla <satosamu@gmail.com>
2024-02-19 17:21:33 +0900

    Reject spammer
    https://github.com/best-friends/mastodon/pull/1941/commits/d3518f49bad2256face1a39a02ecf0647c96da87

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -62,6 +62,13 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
       attach_counts(@status)
     end
 
+    if like_a_spam?
+      @status = nil
+      raise ActiveRecord::Rollback
+    end
+
+    return if @status.nil?
+
     resolve_thread(@status)
     resolve_unresolved_mentions(@status)
     fetch_replies(@status)
@@ -465,5 +472,14 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
   rescue ActiveRecord::StaleObjectError
     poll.reload
     retry
+  end
+
+  def like_a_spam?
+    (
+      !@status.account.local? &&
+      @status.account.followers_count.zero? &&
+      @status.account.created_at > 1.day.ago &&
+      @mentions.count >= 2
+    )
   end
 end

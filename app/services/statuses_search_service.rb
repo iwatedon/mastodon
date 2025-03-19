35f1f28b1db349806861ba626bc0de4cfd71636a
aquarla <satosamu@gmail.com>
2025-03-19 15:31:25 +0900

    Add ad hoc solution for paging issue of StatusesSearchService

--- app/services/statuses_search_service.rb
+++ app/services/statuses_search_service.rb
@@ -8,7 +8,6 @@ class StatusesSearchService < BaseService
       @options = options
       @limit   = options[:limit].to_i
       @offset  = options[:offset].to_i
-      convert_deprecated_options!
 
       span.add_attributes(
         'search.offset' => @offset,
@@ -30,41 +29,13 @@ class StatusesSearchService < BaseService
                     .where('statuses.text &@~ ?', @query)
                     .group(:id)
                     .offset(@offset)
-                    .limit(@limit)
+                    .limit(@limit * 2) # Ad hoc solution for paging issue...
                     .order('statuses.id DESC')
-
-    results = results.where(account_id: @options[:account_id]) if @options[:account_id].present?
-
-    if @options[:min_id].present? || @options[:max_id].present?
-      results = results.where('statuses.id > ?', @options[:min_id]) if @options[:min_id].present?
-      results = results.where('statuses.id < ?', @options[:max_id]) if @options[:max_id].present?
-    end
 
     account_ids         = results.map(&:account_id)
     account_domains     = results.map(&:account_domain)
     preloaded_relations = @account.relations_map(account_ids, account_domains)
 
-    results.reject { |status| StatusFilter.new(status, @account, preloaded_relations).filtered? }
-  end
-
-  def convert_deprecated_options!
-    syntax_options = []
-
-    if @options[:account_id]
-      username = Account.select(:username, :domain).find(@options[:account_id]).acct
-      syntax_options << "from:@#{username}"
-    end
-
-    if @options[:min_id]
-      timestamp = Mastodon::Snowflake.to_time(@options[:min_id].to_i)
-      syntax_options << "after:\"#{timestamp.iso8601}\""
-    end
-
-    if @options[:max_id]
-      timestamp = Mastodon::Snowflake.to_time(@options[:max_id].to_i)
-      syntax_options << "before:\"#{timestamp.iso8601}\""
-    end
-
-    @query = "#{@query} #{syntax_options.join(' ')}".strip if syntax_options.any?
+    results.reject { |status| StatusFilter.new(status, @account, preloaded_relations).filtered? }.first(@limit)
   end
 end
